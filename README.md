779da78a3512f3f5205504667991906d68cdd6f5
South Matic <Southmatic729@gmail.com>
2019-11-06 19:25:28 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # [Burger Logger](https://obscure-meadow-09470.herokuapp.com/)
+
+### Technologies used:
+- Orm
+- Node.js
+- mysql
+- express
+- express-handlebars
--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,26 @@
 var connection = require('../config/connection')
 
 var Orm = {
-  selectAll: function () {
-    var newQuery = 'SELECT * FROM burgers'
-    connection.query(newQuery, function (err, result) {
+  selectAll: function (tableSel) {
+    const newQuery = 'SELECT * FROM ??'
+    connection.query(newQuery,[tableSel], function (err, result) {
       if (err) throw err
       console.table(result)
     })
   },
   insertOne: function () {
-
+    const newQuery = 'INSERT INTO burgers VALUES (?)'
+    connection.query(newQuery, [req.body.burger], function (err, result) {
+      if (err) {
+        return result.status(500).end()
+      }
+    })
   },
   updateOne: function () {
 
+  },
+  deleteOne: function () {
+    const queryString
   }
 }
 
