bb4e42a4dd26926f1354b356ee9410ee9eb9a2d1
South Matic <Southmatic729@gmail.com>
2019-11-10 20:05:55 -0500

    added sound

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,11 +1,14 @@
 
 $(document).ready(function () {
+  var munch = new Audio('./assets/audio/munch-sound-effect.mp3')
+  munch.autoplay=true;
   $('#add').on('click', function () {
     event.preventDefault()
     const burger = $('#burgerName').val().trim()
     if (burger === '' || burger === undefined) {
       alert('Try Again')
     } else {
+      munch.play()
       $.ajax('/api/burgers/' + burger, {
         type: 'POST'
       }).then(function () {
@@ -17,6 +20,7 @@ $(document).ready(function () {
 
   $('.devour').on('click', function () {
     var eaten = this.id
+    munch.play()
     console.log(eaten)
     $.ajax('/api/burgers/' + eaten, {
       type: 'PUT'
